f5519d20721d02ad4b6d818aafb159a913c33f57
SplaatSites <139064374+SplaatSites@users.noreply.github.com>
2026-05-09 11:20:48 -0300

    Update config.go
    Added Pokémon Mini

--- config/config.go
+++ config/config.go
@@ -29,6 +29,7 @@ type Config struct {
 	Gb               GbConfig         `toml:"gb"`
 	Gba              GbaConfig        `toml:"gba"`
 	Nds              NdsConfig        `toml:"nds"`
+	Min              MinConfig        `toml:"min"`
 	KeyboardConfig   KeyboardConfig   `toml:"keyboard"`
 	ControllerConfig ControllerConfig `toml:"controller"`
 	VsyncDisabled    bool             `coml:"vsync_disabled"`
